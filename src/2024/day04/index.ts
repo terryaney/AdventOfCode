5fc370655af73d2214969c0ffaa1deeafd293497
Terry Aney <terry.aney@icloud.com>
2024-12-04 22:27:22 -0600

    Cleaned up Day 4 part 2 based on HyperNeutrino suggestion

--- src/2024/day04/index.ts
+++ src/2024/day04/index.ts
@@ -9,6 +9,7 @@ const solve = (rawInput: string, isPart1: boolean) => {
 	let wordCounts = 0;
 
 	if (isPart1) {
+		/*
 		const directions = [
 			{ dr: 0, dc: 1 },   // Left to Right
 			{ dr: 0, dc: -1 },  // Right to Left
@@ -19,20 +20,34 @@ const solve = (rawInput: string, isPart1: boolean) => {
 			{ dr: 1, dc: -1 },  // Top-Right to Bottom-Left
 			{ dr: -1, dc: 1 }   // Bottom-Left to Top-Right
 		];
-		
+		*/
+
 		for (let r = 0; r < rows; r++) {
 			for (let c = 0; c < cols; c++) {
-				if (inputGrid[r][c] === "X") {
-					for (const { dr, dc } of directions) {
+				if (inputGrid[r][c] != "X") continue;
+
+				// Saw this in HyperNeutrino...'faster' to type in competitions :)
+				/*
+				for (const { dr, dc } of directions) {
+					if (r + dr * 3 < 0 || r + dr * 3 >= rows || c + dc * 3 < 0 || c + dc * 3 >= cols) continue;
+					... same code
+				}
+				*/
+				for (const dr of [-1, 0, 1]) {
+					for (const dc of [-1, 0, 1]) {
+						if (dr === 0 && dc === 0) continue;
+						if (r + dr * 3 < 0 || r + dr * 3 >= rows || c + dc * 3 < 0 || c + dc * 3 >= cols) continue;
+
 						let found = true;
 						for (let i = 1; i < 4; i++) {
 							const nr = r + dr * i;
 							const nc = c + dc * i;
-							if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || inputGrid[nr][nc] !== "XMAS"[i]) {
+							if (inputGrid[nr][nc] !== "XMAS"[i]) {
 								found = false;
 								break;
 							}
 						}
+
 						if (found) {
 							wordCounts++;
 						}
@@ -43,73 +58,96 @@ const solve = (rawInput: string, isPart1: boolean) => {
 	} else {
 		// util.logGrid(inputGrid);
 
-		const directions: Record<string, { movement: { dr: number, dc: number }, pairs: Array<{ key: string, sr: number, sc: number }> }> = {
-			"tlbr": { // Top-Left to Bottom-Right
-				movement: { dr: 1, dc: 1 },
-				pairs: [{ key: "bltr", sr: 2, sc: 0 }, { key: "trbl", sr: 0, sc: 2 }]
-			},
-			"brtl": { // Bottom-Right to Top-Left
-				movement: { dr: -1, dc: -1 },
-				pairs: [{ key: "bltr", sr: 0, sc: -2 }, { key: "trbl", sr: -2, sc: 0 }]
-			},
-			"trbl": { // Top-Right to Bottom-Left
-				movement: { dr: 1, dc: -1 },
-				pairs: [{ key: "tlbr", sr: 0, sc: -2 }, { key: "brtl", sr: 2, sc: 0 }]
-			},
-			"bltr": { // Bottom-Left to Top-Right
-				movement: { dr: -1, dc: 1 },
-				pairs: [{ key: "tlbr", sr: -2, sc: 0 }, { key: "brtl", sr: 0, sc: 2 }]
-			}
-		};
+		// HyperNeutrino's suggestion of searching for "A"
+		for (let r = 1; r < rows - 1; r++) {
+			for (let c = 1; c < cols - 1; c++) {
+				if (inputGrid[r][c] != "A") continue;
 
-		for (let r = 0; r < rows; r++) {
-			for (let c = 0; c < cols; c++) {
-				if (inputGrid[r][c] === "M") {
-					Object.keys(directions).forEach(key => {
-						var direction = directions[key];
-						let found = true;
+				const leftToRight = inputGrid[r - 1][c - 1] + inputGrid[r + 1][c + 1];
+				const rightToLeft = inputGrid[r - 1][c + 1] + inputGrid[r + 1][c - 1];
 
-						for (let i = 1; i < 3; i++) {
-							const nr = r + direction.movement.dr * i;
-							const nc = c + direction.movement.dc * i;
-							if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || inputGrid[nr][nc] !== "MAS"[i]) {
-								found = false;
-								break;
-							}
-						}
-						if (found)
-						{
-							const validPair = direction.pairs.find(pair => {
-								const pairDirection = directions[pair.key];
-								
-								for (let i = 0; i < 3; i++) {
-									const nr = r + pair.sr + pairDirection.movement.dr * i;
-									const nc = c + pair.sc + pairDirection.movement.dc * i;
-									if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || inputGrid[nr][nc] !== "MAS"[i]) {
-										return false;
-									}
-								}
-								
-								return true;
-							});
-
-							if (validPair != undefined) {
-								/*
-								const first = `[${r}, ${c}]:[${r + direction.movement.dr * 2}, ${c + direction.movement.dc * 2}]`
-								const second = `[${r + validPair.sr}, ${c + validPair.sc}]:[${r + validPair.sr + directions[validPair.key].movement.dr * 2}, ${c + validPair.sc + directions[validPair.key].movement.dc * 2}]`
-								console.log(`Found X-MAS as ${first} with ${second}`);
-								*/
-								wordCounts++;	
-							}
-						}
-					});
-
-					inputGrid[r][c] = "."; // visited...
+				if (["MS", "SM"].includes(leftToRight) && ["MS", "SM"].includes(rightToLeft)) {
+					wordCounts++;
 				}
 			}
 		}
 	}
 
+	return wordCounts;
+};
+
+const part2Original = (rawInput: string) => {
+	const inputGrid = util.parseGrid(rawInput);
+	const rows = inputGrid.length;
+	const cols = inputGrid[0].length;
+
+	let wordCounts = 0;
+
+	const directions: Record<string, { movement: { dr: number, dc: number }, pairs: Array<{ key: string, sr: number, sc: number }> }> = {
+		"tlbr": { // Top-Left to Bottom-Right
+			movement: { dr: 1, dc: 1 },
+			pairs: [{ key: "bltr", sr: 2, sc: 0 }, { key: "trbl", sr: 0, sc: 2 }]
+		},
+		"brtl": { // Bottom-Right to Top-Left
+			movement: { dr: -1, dc: -1 },
+			pairs: [{ key: "bltr", sr: 0, sc: -2 }, { key: "trbl", sr: -2, sc: 0 }]
+		},
+		"trbl": { // Top-Right to Bottom-Left
+			movement: { dr: 1, dc: -1 },
+			pairs: [{ key: "tlbr", sr: 0, sc: -2 }, { key: "brtl", sr: 2, sc: 0 }]
+		},
+		"bltr": { // Bottom-Left to Top-Right
+			movement: { dr: -1, dc: 1 },
+			pairs: [{ key: "tlbr", sr: -2, sc: 0 }, { key: "brtl", sr: 0, sc: 2 }]
+		}
+	};
+
+	for (let r = 0; r < rows; r++) {
+		for (let c = 0; c < cols; c++) {
+			if (inputGrid[r][c] === "M") {
+				Object.keys(directions).forEach(key => {
+					var direction = directions[key];
+					let found = true;
+
+					for (let i = 1; i < 3; i++) {
+						const nr = r + direction.movement.dr * i;
+						const nc = c + direction.movement.dc * i;
+						if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || inputGrid[nr][nc] !== "MAS"[i]) {
+							found = false;
+							break;
+						}
+					}
+					if (found)
+					{
+						const validPair = direction.pairs.find(pair => {
+							const pairDirection = directions[pair.key];
+							
+							for (let i = 0; i < 3; i++) {
+								const nr = r + pair.sr + pairDirection.movement.dr * i;
+								const nc = c + pair.sc + pairDirection.movement.dc * i;
+								if (nr < 0 || nr >= rows || nc < 0 || nc >= cols || inputGrid[nr][nc] !== "MAS"[i]) {
+									return false;
+								}
+							}
+							
+							return true;
+						});
+
+						if (validPair != undefined) {
+							/*
+							const first = `[${r}, ${c}]:[${r + direction.movement.dr * 2}, ${c + direction.movement.dc * 2}]`
+							const second = `[${r + validPair.sr}, ${c + validPair.sc}]:[${r + validPair.sr + directions[validPair.key].movement.dr * 2}, ${c + validPair.sc + directions[validPair.key].movement.dc * 2}]`
+							console.log(`Found X-MAS as ${first} with ${second}`);
+							*/
+							wordCounts++;	
+						}
+					}
+				});
+
+				inputGrid[r][c] = "."; // visited...
+			}
+		}
+	}
 	return wordCounts;
 };
 
